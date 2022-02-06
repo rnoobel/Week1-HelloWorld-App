dc75cd96406322354aca66d6114252abdda3408f
Raido Noobel <rnoobel@tktk.ee>
2022-02-06 18:56:55 +0200

    Create Hello World Program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis su nimi on?");
+            string UserName;
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Tere, " + UserName);
+
+            Console.Read();
+
+          
+        }
+    }
+}
